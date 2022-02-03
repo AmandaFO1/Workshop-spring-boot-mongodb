ab65a597a6af2814279805fbe6e67b25c88d9da6
Amanda Ferreira <amandaf1604@gmail.com>
2022-02-03 15:44:40 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/amanda/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.amanda.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
